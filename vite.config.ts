3d7e72da2c3d5f4c82a4218b008288e9eefb39f6
gosble <anthony.ricse98@gmail.com>
2026-06-29 17:22:15 -0500

    feat(*): base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/comerce-web/', 
+  base: 'https://anthonyricse98.github.io/comerce-web/', 
 })
