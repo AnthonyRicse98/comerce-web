612dd9d6ab7b8ac0728dbbf9e518110f9c24df73
gosble <anthony.ricse98@gmail.com>
2026-06-30 23:06:41 -0500

    feat(*): configure deploy yml

--- src/literals.tsx
+++ src/literals.tsx
@@ -1,14 +1,12 @@
 import i18next from 'i18next';
 
 import translationES from '../locales/es.json';
-import translationEN from '../locales/en.json';
 
 i18next.init({
   lng: 'es',
   fallbackLng: 'es',
   resources: {
     es: { translation: translationES },
-    en: { translation: translationEN }
   }
 });
 
